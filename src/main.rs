63308bb873f88ef067dc54530083c7d87c891794
Pavle <vlajkovicpavle123@gmail.com>
2025-03-12 10:26:48 +0100

    feat: FetchAll and Delete Todo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use app::{App, AppResult};
 use event::{Event, EventHandler};
 use handler::handle_key_events;
-use persistence::todo::{Database, Todo};
 use ratatui::{Terminal, prelude::CrosstermBackend};
 use std::io;
 use tui::Tui;
@@ -20,28 +19,22 @@ struct Config {
 
 #[tokio::main]
 async fn main() -> AppResult<()> {
-    let database: Database = Database::new("sqlite://todo.db").await;
-    let todo = Todo {
-        title: "Hello!".to_string(),
-        body: "World!".to_string(),
-    };
-    println!("{:?}", database.insert(todo).await?);
-    // let config = Config { tick_rate: 250 };
-    // let mut app = App::new();
-    // let backend = CrosstermBackend::new(io::stdout());
-    // let terminal = Terminal::new(backend)?;
-    // let events = EventHandler::new(config.tick_rate);
-    // let mut tui = Tui::new(terminal, events);
-    // tui.init()?;
-    // while app.running {
-    //     tui.draw(&mut app)?;
-    //     match tui.events.next().await? {
-    //         Event::Tick => app.tick(),
-    //         Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
-    //         Event::Mouse(_) => {}
-    //         Event::Resize(_, _) => {}
-    //     }
-    // }
-    // tui.exit()?;
+    let config = Config { tick_rate: 250 };
+    let mut app = App::new();
+    let backend = CrosstermBackend::new(io::stdout());
+    let terminal = Terminal::new(backend)?;
+    let events = EventHandler::new(config.tick_rate);
+    let mut tui = Tui::new(terminal, events);
+    tui.init()?;
+    while app.running {
+        tui.draw(&mut app)?;
+        match tui.events.next().await? {
+            Event::Tick => app.tick(),
+            Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+        }
+    }
+    tui.exit()?;
     Ok(())
 }
--- src/persistence/todo.rs
+++ src/persistence/todo.rs
@@ -1,5 +1,5 @@
 use sqlx::{
-    Error, Pool, Sqlite, SqlitePool, migrate::MigrateDatabase, prelude::FromRow, query,
+    Pool, Sqlite, SqlitePool, migrate::MigrateDatabase, prelude::FromRow, query,
     sqlite::SqliteQueryResult,
 };
 
@@ -9,32 +9,53 @@ use sqlx::{
 pub struct Todo {
     pub title: String,
     pub body: String,
+    pub id: i64,
+}
+
+#[derive(Default)]
+pub struct CreateTodo {
+    pub title: String,
+    pub body: String,
 }
 
 pub struct Database {
     connection: Pool<Sqlite>,
 }
 
-// "sqlite://todo.db".to_string()
 impl Database {
     pub async fn new(db_url: &str) -> Database {
         Database {
             connection: {
                 match initialize_db(db_url).await {
                     Ok(connection) => connection,
-                    Err(error) => panic!("Failed to aquire con;ection {:?}", error),
+                    Err(error) => panic!("Failed to aquire connection {:?}", error),
                 }
             },
         }
     }
 
-    pub async fn insert(&self, todo: Todo) -> Result<SqliteQueryResult, sqlx::Error> {
+    pub async fn insert(&self, todo: CreateTodo) -> Result<SqliteQueryResult, sqlx::Error> {
         let query_result = query("INSERT INTO todo (title,body) VALUES( ?, ?)")
             .bind(todo.title)
             .bind(todo.body)
             .execute(&self.connection)
             .await?;
         Ok(query_result)
+    }
+
+    pub async fn fetch_all(&self) -> Result<Vec<Todo>, sqlx::Error> {
+        let results = sqlx::query_as::<_, Todo>("SELECT title,body,id FROM todo")
+            .fetch_all(&self.connection)
+            .await?;
+        Ok(results)
+    }
+
+    pub async fn delete(&self, todo: Todo) -> Result<SqliteQueryResult, sqlx::Error> {
+        let result = sqlx::query("DELETE FROM todo WHERE id=$1")
+            .bind(todo.id)
+            .execute(&self.connection)
+            .await?;
+        Ok(result)
     }
 }
 
@@ -53,8 +74,7 @@ async fn initialize_db(db_url: &str) -> Result<Pool<Sqlite>, sqlx::Error> {
         "CREATE TABLE IF NOT EXISTS todo(
               id INTEGER PRIMARY KEY NOT NULL, 
               title TEXT NOT NULL,
-              body TEXT NOT NULL,
-              created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP)",
+              body TEXT NOT NULL)",
     )
     .execute(&db_connection)
     .await?;
